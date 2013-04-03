938e060b695dcb08c70b1e62f360c79349b1c353
Nelson Manuel Moreno Restrepo <nelsonmanuelmorenorestrepo@gmail.com>
2013-04-03 14:17:54 -0500

    Recomponer repositorio con Ceiba Software como Organización Github.

---
+++ HandsOnDrools/src/co/com/ceiba/edu/drools/fusion/EmergencySignal.java
@@ -0,0 +1,30 @@
+package co.com.ceiba.edu.drools.fusion;
+
+
+public class EmergencySignal {
+
+    private String flight;
+    private long distance;
+
+    public EmergencySignal(String flight, long distance) {
+        this.flight = flight;
+        this.distance = distance;
+    }
+
+    public String getFlight() {
+        return flight;
+    }
+
+    public void setFlight(String flight) {
+        this.flight = flight;
+    }
+
+    public long getDistance() {
+        return distance;
+    }
+
+    public void setDistance(long distance) {
+        this.distance = distance;
+    }
+
+}
